b29cf17e2c7c3814aa9ad9fdded2f88c1dd5999d
xinkev <lyx13710@gmail.com>
2023-03-21 12:14:49 +0630

    Trying with Compose Canvas

--- app/src/main/java/com/xinkev/sign/MainActivity.kt
+++ app/src/main/java/com/xinkev/sign/MainActivity.kt
@@ -1,26 +1,30 @@
 package com.xinkev.sign
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
 import android.os.Bundle
-import android.util.AttributeSet
 import android.view.MotionEvent
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.xinkev.sign.ui.theme.SignatureTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,103 +36,68 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    SignaturePad()
+                    SignaturePad(
+                        brushSize = 1.dp,
+                        brushColor = Color.Black,
+                        modifier = Modifier.fillMaxSize()
+                    )
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun SignaturePad() {
-    AndroidView(
-        modifier = Modifier.fillMaxSize(),
-        factory = { SignaturePad(it) }
-    )
-}
+fun SignaturePad(
+    modifier: Modifier = Modifier,
+    brushSize: Dp,
+    brushColor: Color,
+) {
+    val path = remember { Path() }
+    var draw by remember { mutableStateOf(0) }
+    var currentPosition by remember { mutableStateOf(Offset.Unspecified) }
 
+    Canvas(
+        modifier = modifier.then(
+            Modifier.pointerInteropFilter {
+                when (it.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        path.moveTo(it.x, it.y)
+                        currentPosition = Offset(it.x, it.y)
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        path.quadraticBezierTo(
+                            currentPosition.x,
+                            currentPosition.y,
+                            (currentPosition.x + it.x) / 2,
+                            (currentPosition.y + it.y) / 2
 
-class SignaturePad(
-    context: Context,
-    attributeSet: AttributeSet? = null
-) : View(context, attributeSet) {
-    private val path = Path()
-    private var currentOffset = Offset.Unspecified
-    private val brushPaint = Paint()
-    private val brushSize: Float = 0.1f
-    private val paintColor = Color.BLACK
-    private val canvasPaint = Paint(Paint.DITHER_FLAG)
-    private lateinit var bitmap: Bitmap
-    private lateinit var canvas: Canvas
+                        )
 
-    init {
-        brushPaint.apply {
-            isAntiAlias = true
-            color = paintColor
-            isAntiAlias = true
-            strokeWidth = brushSize
-            style = Paint.Style.STROKE
-            strokeJoin = Paint.Join.ROUND
-            strokeCap = Paint.Cap.ROUND
-        }
-    }
+                        currentPosition = Offset(it.x, it.y)
+                        draw++
+                    }
 
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-        bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
-        canvas = Canvas(bitmap)
-    }
-
-    fun clear() {
-        path.reset()
-        invalidate()
-    }
-
-    override fun onDraw(canvas: Canvas) {
-        canvas.drawBitmap(bitmap, 0f, 0f, canvasPaint)
-        canvas.drawPath(path, brushPaint)
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        val eventX = event.x
-        val eventY = event.y
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                path.moveTo(eventX, eventY)
-                currentOffset = Offset(eventX, eventY)
+                    MotionEvent.ACTION_UP -> {
+                        path.lineTo(it.x, it.y)
+                        currentPosition = Offset.Unspecified
+                    }
+                }
+                true
             }
-            MotionEvent.ACTION_MOVE -> {
-                path.quadTo(
-                    currentOffset.x,
-                    currentOffset.y,
-                    (currentOffset.x + eventX) / 2,
-                    (currentOffset.y + eventY) / 2
-
+        )
+    ) {
+        if (draw > 0) {
+            drawPath(
+                path = path,
+                color = brushColor,
+                style = Stroke(
+                    width = brushSize.toPx(),
+                    cap = StrokeCap.Round,
+                    join = StrokeJoin.Round
                 )
-//                path.lineTo(eventX, eventY)
-                canvas.drawPath(path, brushPaint)
-                currentOffset = Offset(eventX, eventY)
-
-            }
-
-            MotionEvent.ACTION_UP -> {
-                path.lineTo(eventX, eventY)
-                currentOffset = Offset.Unspecified
-            }
-            else -> return false
+            )
         }
-        invalidate()
-        return true
     }
 }
-
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SignatureTheme {
-        SignaturePad()
-    }
-}
-
